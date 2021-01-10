11de082f763d29d15d22d5123803584cbd43696c
Jose Joaquin Lopez Sanchez <jojolosa@outlook.es>
2021-01-10 14:53:52 +0100

    implementacion metodo Bottom Up

--- src/main/java/org/jose/F.java
+++ src/main/java/org/jose/F.java
@@ -8,12 +8,13 @@ public class F {
     public static void main(final String[] args) {
 
         int h = 0;
-        System.out.println("HOLA,ADIOS");
+        int j = 0;
         final F jl = new F();
         long start = System.currentTimeMillis();
         h = jl.fibonaccirecursivo(Integer.parseInt(args[0]));
         System.out.println("Recursive: " + h + " in "
-            + (System.currentTimeMillis() - start) + "ms");
+                + (System.currentTimeMillis() - start) + "ms");
+
 
     }
 
@@ -26,6 +27,27 @@ public class F {
             } else {
                 throw new RuntimeException("Error de calculo con: " + n);
             }
+        }
+    }
+
+    private int fibonaccibottom_up(int n) {
+        int resultado = 1;
+        int nmenos1 = 1;
+        int nmenos2 = 1;
+        if (n == 1 || n == 0) {
+            return 1;
+        } else {
+            if (n > 0) {
+                for (int i = 2; i < n; i++) {
+                    resultado = nmenos1 + nmenos2;
+                    nmenos2 = nmenos1;
+                    nmenos1 = resultado;
+                }
+                return resultado;
+            } else {
+                throw new RuntimeException("Error de calculo con: " + n);
+            }
+
         }
     }
 
