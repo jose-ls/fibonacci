d1771d91cc13e8aa0755b684864f0987a1937055
Jose Joaquin Lopez Sanchez <jojolosa@outlook.es>
2021-02-01 12:12:40 +0100

    Implementacion del menu

--- src/main/java/org/jose/Base.java
+++ src/main/java/org/jose/Base.java
@@ -1,5 +1,8 @@
 package org.jose;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Base {
     /**
      * Entry point.
@@ -7,15 +10,73 @@ public class Base {
      */
     public static void main(final String[] args) {
         Base objeto = new Base();
-        int numero = Integer.parseInt(args[0]);
-        objeto.calcularTiempo(numero, new FibonacciBottomUp());
-        objeto.calcularTiempo(numero, new FibonacciRecursivo());
-        objeto.calcularTiempo(numero, new FibonacciRecursivoMemorizacion());
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+        while (!salir) {
+            System.out.println("Elige que método usar " +
+                    "para calcular fibonacci");
+            System.out.println(" ");
+            System.out.println("     1. BottomUp ");
+            System.out.println("     2. Recursivo ");
+            System.out.println("     3. Recursivo con Memoria ");
+            System.out.println(" ");
+            System.out.println("     4. Salir ");
+
+            try {
+                opcion = sn.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        objeto.calcularTiempo(objeto.escogerNumero(),
+                                new FibonacciBottomUp());
+                        break;
+                    case 2:
+                        objeto.calcularTiempo(objeto.escogerNumero(),
+                                new FibonacciRecursivo());
+                        break;
+                    case 3:
+                        objeto.calcularTiempo(objeto.escogerNumero(),
+                                new FibonacciRecursivoMemorizacion());
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo hay cuatro opciones, " +
+                                "elige entre 1,2,3 y 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un numero");
+                sn.next();
+            }
+        }
     }
 
     private void calcularTiempo(final int numero,
                                 final FibonacciConMetodo tipo) {
         FibonacciConTiempo calculado = new FibonacciConTiempo(tipo);
         System.out.println("El resultado es: " + calculado.calcular(numero));
+    }
+
+    private int escogerNumero() {
+        int numero = -1;
+        Scanner sn = new Scanner(System.in);
+        while (numero < 0) {
+            System.out.println("¿Qué número quieres calcular? ");
+            numero = sn.nextInt();
+            try {
+                if (numero < 0) {
+                    System.out.println("Inserta un numero mayor que 0");
+                    sn.next();
+                    numero = sn.nextInt();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un numero");
+                sn.next();
+                numero = sn.nextInt();
+            }
+        }
+        return numero;
     }
 }
